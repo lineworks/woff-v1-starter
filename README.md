07bcf2fc603d3e21011d636f175d01a42608dd9d
INHO JUNG <inojng@gmail.com>
2021-06-04 17:33:25 +0900

    first upload woff-v1-starter

---
+++ README.md
@@ -0,0 +1,3 @@
+# WOFF v1 starter app
+
+This is a small web application that demonstrates the basic functionality of the WORKS Front-end Framework (WOFF).
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const myWoffId = process.env.MY_LIFF_ID;
+
+app.use(express.static('public'));
+
+app.get('/send-id', function(req, res) {
+    res.json({id: myWoffId});
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
